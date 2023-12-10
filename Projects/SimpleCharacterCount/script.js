e2d3d1a938f3377e27a30748479e0e80acbe174e
RANJITH KUMAR VENKATACHALAM <ratamranjith@gmail.com>
2023-12-11 00:51:41 +0530

    Display Profile outline animation

---
+++ Projects/SimpleCharacterCount/script.js
@@ -0,0 +1,5 @@
+function characterCounter(){
+    characters = document.getElementsByClassName(".charCounter");
+    console.log(characters);
+    document.querySelector('.charCounter').style.color = 'red'
+}
